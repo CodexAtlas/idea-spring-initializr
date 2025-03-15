10ea0dacfb0e7f72fe35c786b2d2882d694107bc
CodexAtlas <cncodexatlas@gmail.com>
2025-03-16 00:29:57 +0800

    启动类默认扫描规则测试

--- boot3-demo-03/src/test/java/com/example/runStart/MainApplicationTest.java
+++ boot3-demo-03/src/test/java/com/example/runStart/MainApplicationTest.java
@@ -1,15 +1,16 @@
-package example.runStartTest;
+package com.example.runStart;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureJdbc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest(classes = MainApplicationTest.class)
-@AutoConfigureJdbc
+@SpringBootTest
+// 如果不在com.example包下，则需要指定启动类，因为启动类默认扫描com.example包及其子包，违反默认扫描规则
+//@SpringBootTest(classes = MainApplicationTest.class)
+//@AutoConfigureJdbc
 public class MainApplicationTest {
 
     @Autowired
